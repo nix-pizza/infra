e8a026a3c22b0eacb44ef228ca8e6089cc6ad9e2
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-04-29 22:20:32 +0200

    Remove `ananas.nix.pizza`

--- hosts/nix-pizza/nginx.nix
+++ hosts/nix-pizza/nginx.nix
@@ -12,18 +12,5 @@
     recommendedTlsSettings = true;
   };
 
-  # Just for testing
-  services.nginx.virtualHosts."ananas.nix.pizza" = {
-    default = true;
-    enableACME = true;
-    forceSSL = true;
-    locations = {
-      "/" = {
-        return = ''200 "Pizza con l'ananas"'';
-        extraConfig = "add_header Content-Type text/plain;";
-      };
-    };
-  };
-
   networking.firewall.allowedTCPPorts = [ 80 443 ];
 }
