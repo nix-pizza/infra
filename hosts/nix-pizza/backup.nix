0d40d785c6fa5afe3fcb3c69e4f42be757b06a60
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-05-09 00:47:40 +0200

    Add hetzner storage box key between known hosts
    Fix restic backups

--- hosts/nix-pizza/backup.nix
+++ hosts/nix-pizza/backup.nix
@@ -1,4 +1,10 @@
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, lib, ... }:
+let
+  user = "u382036-sub2";
+  host = "u382036.your-storagebox.de";
+  port = "23";
+in
+{
   age.secrets = {
     HETZNER_STORAGE_BOX_SSH_PASSWORD = {
       file = ../../secrets/HETZNER_STORAGE_BOX_SSH_PASSWORD.age;
@@ -10,21 +16,17 @@
     };
   };
 
-  services.restic.backups.nix-pizza =
-    let
-      user = "u382036-sub2";
-      host = "u382036.your-storagebox.de";
-      port = "23";
-    in
-    {
-      paths = [ "/persist" ];
-      passwordFile = config.age.secrets.NIX_PIZZA_RESTIC_PASSWORD.path;
-      extraOptions = [
-        "sftp.command='${lib.getExe pkgs.sshpass} -f ${config.age.secrets.HETZNER_STORAGE_BOX_SSH_PASSWORD.path} ssh -p${port} ${user}@${host} -s sftp'"
-      ];
-      repository = "sftp://${user}@${host}:${port}/";
-      initialize = true;
-      timerConfig.OnCalendar = "daily";
-      timerConfig.RandomizedDelaySec = "1h";
-    };
+  services.openssh.knownHosts."${host}".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIICf9svRenC/PLKIL9nk6K/pxQgoiFC41wTNvoIncOxs";
+
+  services.restic.backups.nix-pizza = {
+    paths = [ "/persist" ];
+    passwordFile = config.age.secrets.NIX_PIZZA_RESTIC_PASSWORD.path;
+    extraOptions = [
+      "sftp.command='${lib.getExe pkgs.sshpass} -f ${config.age.secrets.HETZNER_STORAGE_BOX_SSH_PASSWORD.path} ssh -p${port} ${user}@${host} -s sftp'"
+    ];
+    repository = "sftp://${user}@${host}:${port}/";
+    initialize = true;
+    timerConfig.OnCalendar = "daily";
+    timerConfig.RandomizedDelaySec = "1h";
+  };
 }
