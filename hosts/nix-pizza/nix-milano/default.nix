ac40da90bc54831d15ac5b997897ef041b1789c2
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-05-18 17:49:54 +0200

    `nix-milano` web page

---
+++ hosts/nix-pizza/nix-milano/default.nix
@@ -0,0 +1,20 @@
+{ pkgs, lib, ... }:
+let
+  markdown = ./nix-milano.md;
+  css = ./nix-milano.css;
+  nix-milano = pkgs.runCommandNoCC "nix-milano" { } ''
+    mkdir $out
+    cd $out
+    cp ${css} nix-milano.css
+    ${lib.getExe pkgs.pandoc} --from markdown --css nix-milano.css -s ${markdown} -o index.html --metadata title="Nix Milano"
+  '';
+in
+{
+  services.nginx = {
+    virtualHosts."milano.nix.pizza" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/".root = nix-milano;
+    };
+  };
+}
---
+++ hosts/nix-pizza/nix-milano/nix-milano.md
@@ -0,0 +1,10 @@
+![](https://secure.meetupstatic.com/photos/event/9/c/b/a/clean_519400122.webp)
+
+# Meetup
+Per vedere i prossimi eventi organizzati o iscriverti ci trovi [su meetup](https://www.meetup.com/nix-milano/).
+
+# Matrix
+Unisciti al [canale Matrix](https://matrix.to/#/#nix-milan:matrix.org).
+
+# Telegram
+Unisciti al [gruppo Telegram](https://t.me/nixMilan).
